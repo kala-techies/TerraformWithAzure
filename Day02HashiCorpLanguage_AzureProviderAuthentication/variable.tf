26fc79b3475b1318cebceed831baae927b5d2489
kalandar <shaik.kalandar20@gmail.com>
2024-10-21 16:04:41 +0530

    variable.tf_file_modified

---
+++ Day02HashiCorpLanguage_AzureProviderAuthentication/variable.tf
@@ -0,0 +1,10 @@
+variable "rg_name" {
+    type = string
+    description = "refers to resource group name"
+}
+
+
+variable "rg_loc" {
+    type = string
+    description = "refers to resource group location"
+}
